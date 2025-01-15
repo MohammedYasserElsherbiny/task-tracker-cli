1ebc573f6aff6f53e7f930e9bd583e7759678dd9
MohammedYasserELsherbiny <mohammed.ys2003@gmail.com>
2025-01-15 22:33:37 +0200

    creating the basic idea of update, and delete opreations.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
+using System.Threading.Tasks;
 namespace task_cli
 {
     internal class Program
@@ -33,6 +34,61 @@ namespace task_cli
         // 0 for not done, 1 for in progress, 2 for done
 
     }
+
+    public class TaskOpreations
+    {
+        public void update(int Id, string description, List<Task> tasks)
+        {
+            int isExisted = 0;
+            foreach (var task in tasks)
+            {
+                if (task.Id == Id)
+                {
+                    isExisted++;
+                    task.Description = description;
+                    break;
+                }
+            }
+
+            if (isExisted > 0)
+            {
+                Console.WriteLine("Task was updated tasks.RemoveAt(taskIdx);");
+            }
+            else
+            { 
+                Console.WriteLine("There is no task with this ID"); 
+            }
+        }
+
+        public void delete(int Id, List<Task> tasks)
+        {
+            int taskIdx = -1;
+            for (int i = 0; i < tasks.Count(); i++)
+            {
+                if (tasks[i].Id == Id)
+                {
+                    taskIdx = i;
+                    break;
+                }
+            }
+
+            if(taskIdx > -1)
+            {
+                tasks.RemoveAt(taskIdx);
+                Console.WriteLine("Task was deleted tasks.RemoveAt(taskIdx);"
+            }
+            else
+            {
+                Console.WriteLine("There is no task with this ID");
+            }
+        }
+
+        public void mark(int Id, string type , List<Task> tasks)
+        {
+            //TODO
+        }
+    }
+
 
     public class JsonFileHandler
     {
